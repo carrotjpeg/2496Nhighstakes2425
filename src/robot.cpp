68557c74771688161af78edab82a3a79c5556992
carro._.t <evanathomson@gmail.com>
2024-09-04 22:46:25 -0700

    meow

--- src/robot.cpp
+++ src/robot.cpp
@@ -7,13 +7,14 @@
 #include "pros/motors.h"
 
 // 2496v
-#define LF_PORT 20;
-#define LM_PORT 19;
-#define LB_PORT 18;
-#define RF_PORT 17;
-#define RM_PORT 16;
-#define RB_PORT 15;
-#define INTAKE_PORT 9;
+#define LF_PORT 20 // no semicolons
+#define LM_PORT 19
+#define LB_PORT 18
+#define RF_PORT 17
+#define RM_PORT 16
+#define RB_PORT 15
+#define INTAKE_PORT 9
+#define IMU_PORT 1
 
 // rotation sensor
 // pros::Rotation roto(ROTO_PORT);
